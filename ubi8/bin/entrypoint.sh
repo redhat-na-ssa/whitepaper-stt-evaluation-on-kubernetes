1f6146f2c062ed242ac0519ca26b7b98a0ec7e87
Cory Latschkowski <koree@redhat.com>
2025-02-07 15:23:18 -0600

    updates

--- ubi8/bin/entrypoint.sh
+++ ubi8/bin/entrypoint.sh
@@ -1,35 +1,57 @@
 #!/bin/sh
 
-AUDIO_FILES=${1:-/audio}
-WHISPER_MODEL=${2:-tiny.en}
-MODEL_CACHE=${MODEL_CACHE:-/models}
+AUDIO_FILES=${AUDIO_FILES:-/data/audio}
+WHISPER_MODEL=${WHISPER_MODEL:-tiny.en}
+MODEL_CACHE=${MODEL_CACHE:-/data/models}
+XDG_CACHE_HOME=${MODEL_CACHE}
+
+export XDG_CACHE_HOME
+
+init(){
+  [ -e "${XDG_CACHE_HOME}" ] || mkdir -p "${XDG_CACHE_HOME}"
+}
 
 usage(){
   echo "
     This container processes audio files in the following path and model.
-    You can override these variables:
-
-      AUDIO_FILES: $AUDIO_FILES
+    
+    You can override these (default) environment variables:
+      AUDIO_FILES:   $AUDIO_FILES
+      MODEL_CACHE:   $MODEL_CACHE
       WHISPER_MODEL: $WHISPER_MODEL
 
-    Example: podman run -it --rm -v $(pwd)/audio:/audio whisper process_audio /audio tiny.en
+    Example (interactive): 
+      podman run -it --rm -v \$(pwd)/scratch:/data:z whisper /bin/sh
+
+    Example (batch):
+      podman run -it --rm -v \$(pwd)/scratch:/data:z whisper process_audio
+  
+    Print model list:
+      python3 -c 'import whisper,pprint; pprint.pprint(whisper._MODELS)'
   "
 }
 
 # take the first parameter or default to /audio, then default the model to tiny.en if none passed
 process_audio(){
-  AUDIO_FILES=${1:-/audio}
-  WHISPER_MODEL=${2:-tiny.en}
+  WHISPER_MODEL=${1:-tiny.en}
+  AUDIO_FILES=${2:-/data/audio}
+
+  echo "
+    WHISPER_MODEL: $WHISPER_MODEL
+    AUDIO_FILES: $AUDIO_FILES
+  "
 
   whisper \
-    --model_dir $MODEL_CACHE \
     --model $WHISPER_MODEL \
     $AUDIO_FILES/*
 }
 
+init
+
 # if you pass parameter, it will execute as is, else run whisper --help
 if [ "$1" != "" ]; then
   case "$1" in
+    process_audio) process_audio "$2" "$3";;
     *) exec "$@";;
   esac
 else
