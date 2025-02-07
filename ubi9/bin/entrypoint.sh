c52f17433f9da1f76e10ebd52308203fbd1c104e
dmarcus-wire <dmarcus@redhat.com>
2025-02-07 13:30:22 -0700

    ubi9 test

---
+++ ubi9/bin/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+AUDIO_FILES=${1:-/audio}
+WHISPER_MODEL=${2:-tiny.en}
+
+usage(){
+  echo "
+    This container processes audio files in the following path and model.
+    You can override these variables:
+
+      AUDIO_FILES: $AUDIO_FILES
+      WHISPER_MODEL: $WHISPER_MODEL
+
+    Example command: podman run -it --rm -v $(pwd)/audio:/audio whisper
+  "
+}
+
+# take the first parameter or default to /audio, then default the model to tiny.en if none passed
+process_audio(){
+  AUDIO_FILES=${1:-/audio}
+  WHISPER_MODEL=${2:-tiny.en}
+
+  whisper $AUDIO_FILES --model $WHISPER_MODEL
+}
+
+# if you pass parameter, it will execute as is, else run whisper --help
+if [ "$1" != "" ]; then
+  case "$1" in
+
+    *) exec "$@";;
+  esac
+else
+  usage
+fi
+
