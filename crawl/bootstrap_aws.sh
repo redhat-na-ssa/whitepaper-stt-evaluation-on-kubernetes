ce1bb597e3fd57ba0ae356f19e5e3f1d5fc501e3
Cory Latschkowski <koree@redhat.com>
2025-04-02 16:35:17 -0500

    update: script

--- crawl/bootstrap_aws.sh
+++ crawl/bootstrap_aws.sh
@@ -96,7 +96,7 @@ aws_get_ec2_rhel_hostname(){
   # get instance dns name
   EC2_HOSTNAME=$(aws ec2 describe-instances \
     --filter "Name=tag:Name,Values=${INSTANCE_NAME}" \
-    --filter "Name=instance-state-name,Values=running,stopped" \
+    --filter "Name=instance-state-name,Values=running" \
     --filter "Name=instance-state-name,Values=stopped" \
     --query 'Reservations[].Instances[].PublicDnsName' \
     --output text)
